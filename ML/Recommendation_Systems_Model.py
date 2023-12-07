ba583e3637b8eb39a4e3b6f3b8661a90c22a43e5
Restyled.io <commits@restyled.io>
2023-12-07 00:56:41 +0000

    Restyled by autopep8

--- ML/Recommendation_Systems_Model.py
+++ ML/Recommendation_Systems_Model.py
@@ -1,3 +1,13 @@
+from tensorflow.keras.callbacks import EarlyStopping
+import joblib
+from google.colab import files
+from sklearn.metrics.pairwise import linear_kernel
+from tensorflow.keras.layers import Dense, Embedding, Flatten
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import numpy as np
 import pandas as pd
 import requests
 from io import StringIO
@@ -27,35 +37,30 @@ df_wisata.shape
 # dataset info
 print(df_wisata.info())
 
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, Flatten
 
 df_wisata['City'].head(5)
 
-#Define a TF-IDF Vectorizer Object
+# Define a TF-IDF Vectorizer Object
 tfidf = TfidfVectorizer(stop_words='english')
 
-#Replace NaN with an empty string
+# Replace NaN with an empty string
 df_wisata['City'] = df_wisata['City'].fillna('')
 
-#Construct the required TF-IDF matrix by fitting and transforming the data
+# Construct the required TF-IDF matrix by fitting and transforming the data
 tfidf_matrix = tfidf.fit_transform(df_wisata['City'])
 
-#Output the shape of tfidf_matrix
+# Output the shape of tfidf_matrix
 tfidf_matrix.shape
 
 # Import linear_kernel
-from sklearn.metrics.pairwise import linear_kernel
 
 # Compute the cosine similarity matrix
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-#Construct a reverse map of indices and Place_Name
-indices = pd.Series(df_wisata.index, index=df_wisata['Place_Name']).drop_duplicates()
+# Construct a reverse map of indices and Place_Name
+indices = pd.Series(
+    df_wisata.index, index=df_wisata['Place_Name']).drop_duplicates()
+
 
 def rekomendasi_tempat_wisata(Place_Name, cosine_sim=cosine_sim):
     # Get the index of the destination that matches the Place_Name
@@ -65,7 +70,8 @@ def rekomendasi_tempat_wisata(Place_Name, cosine_sim=cosine_sim):
     similarity_scores = list(enumerate(cosine_sim[idx]))
 
     # Sort the destinations based on the similarity scores
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    similarity_scores = sorted(
+        similarity_scores, key=lambda x: x[1], reverse=True)
 
     # Get the scores of the 15 most similar destinations
     similarity_scores = similarity_scores[1:16]
@@ -74,25 +80,28 @@ def rekomendasi_tempat_wisata(Place_Name, cosine_sim=cosine_sim):
     similar_places_indices = [i[0] for i in similarity_scores]
 
     # Exclude the input Place_Name from the recommendations
-    similar_places_indices = [i for i in similar_places_indices if df_wisata['Place_Name'].iloc[i] != Place_Name]
+    similar_places_indices = [
+        i for i in similar_places_indices if df_wisata['Place_Name'].iloc[i] != Place_Name]
 
     # Return the most similar destinations (Place_Name and City)
     return df_wisata[['Place_Name', 'City']].iloc[similar_places_indices]
 
-from tensorflow.keras.callbacks import EarlyStopping
 
 # Split the data into training and testing sets
-train_data, test_data = train_test_split(df_wisata, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(
+    df_wisata, test_size=0.2, random_state=42)
+
 
 class myCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs={}):
+    def on_epoch_end(self, epoch, logs={}):
 
-  # Check the loss
-    if(logs.get('accuracy') > 0.98):
+        # Check the loss
+        if (logs.get('accuracy') > 0.98):
 
-      # Stop if threshold is met
-      print("\nAccuracy is higher than 0.98 so canceling training!")
-      self.model.stop_training = True
+            # Stop if threshold is met
+            print("\nAccuracy is higher than 0.98 so canceling training!")
+            self.model.stop_training = True
+
 
 callbacks = myCallback()
 
@@ -107,14 +116,16 @@ model.add(Dense(tfidf_matrix.shape[1], activation='linear'))
 model.compile(optimizer=tf.optimizers.Adam(), loss='mse', metrics=['accuracy'])
 
 # Train the model
-model.fit(tfidf_matrix.toarray(), tfidf_matrix.toarray(), epochs=10, batch_size=32, validation_split=0.2, callbacks=[callbacks])
+model.fit(tfidf_matrix.toarray(), tfidf_matrix.toarray(), epochs=10,
+          batch_size=32, validation_split=0.2, callbacks=[callbacks])
 
 # Evaluate the model
 loss, accuracy = model.evaluate(tfidf_matrix.toarray(), tfidf_matrix.toarray())
 print(f'Model Loss: {loss}, Model Accuracy: {accuracy}')
 
 # Validation using rekomendasi_tempat_wisata function
-validation_result = rekomendasi_tempat_wisata('Taman Mini Indonesia Indah (TMII)')
+validation_result = rekomendasi_tempat_wisata(
+    'Taman Mini Indonesia Indah (TMII)')
 print('Rekomendasi tempat wisata :')
 print(validation_result)
 
@@ -122,11 +133,9 @@ print(validation_result)
 model.save("Recommendation_Systems_Model.h5")
 
 # Download the model
-from google.colab import files
 files.download("Recommendation_Systems_Model.h5")
 
 # Save model .pkl
-import joblib
 
 model_filename = "Recommendation_Systems_Model.pkl"
 joblib.dump(model, model_filename)
@@ -136,7 +145,6 @@ print(f"Model saved as {model_filename}")
 loss, accuracy = model.evaluate(tfidf_matrix.toarray(), tfidf_matrix.toarray())
 print(f'Model Loss: {loss}, Model Accuracy: {accuracy}')
 
-from google.colab import files
 
 # Download the model file
 files.download(model_filename)
